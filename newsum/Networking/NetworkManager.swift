6011931d4e68e16892e7a6b649a4e3472e2ba68d
Li Li <harrylili008@gmail.com>
2025-04-17 02:16:13 -0400

    Update fetch api from gcp

--- newsum/Networking/NetworkManager.swift
+++ newsum/Networking/NetworkManager.swift
@@ -9,7 +9,7 @@ enum NetworkError: Error {
 
 class NetworkManager {
     static let shared = NetworkManager()
-    private let baseURL = "http://192.168.1.196:8000/api/summary/latest"
+    private let baseURL = "http://34.21.101.88:8080/api/summary/latest"
 
     private init() {}
 
